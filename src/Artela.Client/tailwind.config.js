dad3169ad5f6d3367c1c4661a8bf0a9a9f4dec8d
alireza2125 <alireza.mahdavi2125@outlook.com>
2022-05-27 11:02:38 +0430

    new version of artela

---
+++ src/Artela.Client/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        "./**/*.{razor,html,ts,css}",
+        "!./wwwroot/**/*",
+        "!/node_modules/**/*"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
