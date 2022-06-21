f92cc4b3b1e845373d1e78a8c9a03372fa10cd9f
alireza2125 <alireza.mahdavi2125@outlook.com>
2022-06-21 19:55:41 +0430

    fix

--- src/Artela.Client/Shared/SplashManager.razor.js
+++ src/Artela.Client/Shared/SplashManager.razor.js
@@ -1,11 +1,9 @@
 export function show() {
     var _a;
     (_a = document.getElementById("splash")) === null || _a === void 0 ? void 0 : _a.classList.remove("hidden");
-    console.log("show splasg");
 }
 export function hide() {
     var _a;
     (_a = document.getElementById("splash")) === null || _a === void 0 ? void 0 : _a.classList.add("hidden");
-    console.log("hide splasg");
 }
 //# sourceMappingURL=SplashManager.razor.js.map
--- src/Artela.Client/Shared/SplashManager.razor.ts
+++ src/Artela.Client/Shared/SplashManager.razor.ts
@@ -1,9 +1,7 @@
 export function show(): void {
     document.getElementById("splash")?.classList.remove("hidden");
-    console.log("show splasg");
 }
 
 export function hide(): void {
     document.getElementById("splash")?.classList.add("hidden");
-    console.log("hide splasg");
 }
